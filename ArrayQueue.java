3cf778b4b5133a60b62d876852e433049740591c
Landon Davidson <accounts@landond.tech>
2025-01-10 21:23:05 -0800

    Add: Skeleton methods

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -1,3 +1,27 @@
 public class ArrayQueue<T> implements MyQueue<T>{
-    
+
+    @Override
+    public void enqueue(T item) {
+
+    }
+
+    @Override
+    public T dequeue() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 }
--- LinkedQueue.java
+++ LinkedQueue.java
@@ -1,5 +1,30 @@
 public class LinkedQueue<E> implements MyQueue<E>{
-    
+
+    @Override
+    public void enqueue(E item) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
     private static class ListNode<E>{
         private final E data;
         private ListNode<E> next;
