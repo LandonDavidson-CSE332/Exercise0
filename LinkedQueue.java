f5d299894ae706e81901a2578f9c1be6dca536e1
Landon Davidson <accounts@landond.tech>
2025-01-10 21:39:25 -0800

    Add: constructor

--- LinkedQueue.java
+++ LinkedQueue.java
@@ -1,8 +1,17 @@
 public class LinkedQueue<E> implements MyQueue<E>{
 
+    private ListNode<E> head;
+    private ListNode<E> tail;
+    private int size = 0;
+
+    public LinkedQueue() {
+        this.head = new ListNode<>(null);
+        this.tail = this.head;
+    }
+
     @Override
     public void enqueue(E item) {
-
+        this.tail = new ListNode<>(item, this.tail);
     }
 
     @Override
@@ -17,7 +26,7 @@ public class LinkedQueue<E> implements MyQueue<E>{
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
