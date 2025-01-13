93b10f0aee5d3c036b824df2bc541b895fda2e01
Landon Davidson <accounts@landond.tech>
2025-01-12 17:13:20 -0800

    Fix: ordering of linked list to make operations constant time

--- LinkedQueue.java
+++ LinkedQueue.java
@@ -1,25 +1,34 @@
 public class LinkedQueue<E> implements MyQueue<E>{
 
+    // Initialize head and tail variables, the linked list points from head to tail
     private ListNode<E> head = null;
     private ListNode<E> tail = null;
     private int size = 0;
 
     @Override
     public void enqueue(E item) {
-        this.tail = new ListNode<>(item, this.tail);
+        // If the list is empty set head and tail to the new node
         if (this.size == 0) {
+            this.tail = new ListNode<>(item);
             this.head = this.tail;
+            this.size++;
+            return;
         }
-        size++;
+        // Create new node after tail and then assign that as the new tail
+        this.tail.next = new ListNode<>(item);
+        this.tail = this.tail.next;
+        this.size++;
     }
 
     @Override
     public E dequeue() {
+        // If stack is empty throw an error
         if (this.size == 0) {
             throw new IllegalStateException();
         }
+        // Save the data in head and then move the head one node up the list
         E data = this.head.data;
-        this.head = null; //TODO
+        this.head = this.head.next;
         return data;
     }
 
