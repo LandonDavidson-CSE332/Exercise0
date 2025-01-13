b9a19153e9ac1dc5f26dcac3fe5a237e91752080
Landon Davidson <accounts@landond.tech>
2025-01-12 17:20:55 -0800

    Refactor: Add more comments

--- LinkedStack.java
+++ LinkedStack.java
@@ -19,36 +19,46 @@ public class LinkedStack<T> implements MyStack<T> {
     private ListNode<T> head = null;
     private int size = 0;
 
+    // Add data to head of stack
     @Override
     public void push(T data) {
+        // Create new node with data and pointer to head and assign it to head
         this.head = new ListNode<>(data, this.head);
         this.size++;
     }
 
+    // Return value at head and remove it from stack
     @Override
     public T pop() {
+        // If list is empty throw an error
         if (this.size == 0) {
             throw new IllegalStateException();
         }
         this.size--;
+        // Read data in head then set head equal to head.next and return data
         T data = this.head.data;
         this.head = this.head.next;
         return data;
     }
 
+    // Return value at head without popping
     @Override
     public T peek() {
+        // If list is empty throw error
         if (this.size == 0) {
             throw new IllegalStateException();
         }
+        // Read and return data in head
         return this.head.data;
     }
 
+    // Return this.size
     @Override
     public int size() {
         return this.size;
     }
 
+    // Return true if this.size == 0
     @Override
     public boolean isEmpty() {
         return this.size == 0;
