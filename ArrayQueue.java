c2dc4386b472286b280df157a9d7310f89b2a47c
Landon Davidson <accounts@landond.tech>
2025-01-13 12:40:05 -0800

    Add: store size in private member instead of calculating

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -1,34 +1,35 @@
 public class ArrayQueue<T> implements MyQueue<T>{
 
-    // Initialize queue array and head and tail indices
+    // Initialize queue array, head and tail indices, and size value
     private T[] queue = (T[]) new Object[10];
     private int head = 5;
     private int tail = 5;
-    private boolean empty = true;
+    private int size = 0;
 
     // Add item to queue at tail
     @Override
     public void enqueue(T item) {
-        // If queue is full then resize, an empty array looks like a full one so we need the empty boolean
-        if (!this.empty && this.tail == this.head) {
+        // If queue is full then resize
+        if (this.size == this.queue.length) {
             resize();
         }
-        // Queue isn't empty since we are adding an element
-        this.empty = false;
         // Put item at the empty tail index then iterate and loop tail by 1 for next enqueue
         queue[this.tail] = item;
         this.tail = (this.tail + 1) % this.queue.length;
+        this.size++;
     }
 
     // Copy queue to the middle of a new queue twice the size
     private void resize() {
         T[] new_queue = (T[]) new Object[this.queue.length * 2];
         // iterate from head to tail of queue
-        for (int i = 0; i < this.tail; i++) {
+        for (int i = 0; i < this.queue.length; i++) {
             // Put head at the start of the second quarter of the new queue,
             // ending at the end of the third quarter since new_queue is twice the size of queue
             new_queue[i + new_queue.length / 4] = this.queue[(this.head + i) % this.queue.length];
         }
+        this.head = new_queue.length / 4;
+        this.tail = this.head + this.queue.length;
         this.queue = new_queue;
     }
 
@@ -36,12 +37,12 @@ public class ArrayQueue<T> implements MyQueue<T>{
     @Override
     public T dequeue() {
         // If empty throw error
-        if (this.empty) {
+        if (this.size == 0) {
             throw new IllegalStateException();
         }
         T data = this.queue[this.head];
-        this.head++;
-        this.empty = this.head == this.tail;
+        this.head = (this.head + 1) % this.queue.length;
+        this.size--;
         return data;
     }
 
@@ -49,7 +50,7 @@ public class ArrayQueue<T> implements MyQueue<T>{
     @Override
     public T peek() {
         // If empty throw error
-        if (this.empty) {
+        if (this.size == 0) {
             throw new IllegalStateException();
         }
         // Return value at head position in queue
@@ -59,14 +60,14 @@ public class ArrayQueue<T> implements MyQueue<T>{
     // Return number of elements in queue
     @Override
     public int size() {
-        // Return difference between tail and head and then mod by length to account for circularity
-        return (this.tail - this.head) % this.queue.length;
+        // Return size
+        return this.size;
     }
 
     // Return true if queue is empty
     @Override
     public boolean isEmpty() {
         // Since we already need to store an empty boolean we can just return that
-        return this.empty;
+        return this.size == 0;
     }
 }
