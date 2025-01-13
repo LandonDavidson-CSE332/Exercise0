780126e006cfba031e468bdd40ef1e37ed9ebdbd
Landon Davidson <accounts@landond.tech>
2025-01-12 17:00:00 -0800

    Add: resize()

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,8 +1,21 @@
 public class ArrayStack<T> implements MyStack<T> {
 
+    private int head = -1;
+    private T[] stack = (T[]) new Object[10];
+
     @Override
     public void push(T item) {
+        if (this.head == this.stack.length - 1) {
+            resize();
+        }
+        this.head++;
+        this.stack[this.head] = item;
+    }
 
+    private void resize() {
+        T[] new_stack = (T[]) new Object[this.stack.length * 2];
+        System.arraycopy(this.stack, 0, new_stack, 0, this.stack.length);
+        this.stack = new_stack;
     }
 
     @Override
